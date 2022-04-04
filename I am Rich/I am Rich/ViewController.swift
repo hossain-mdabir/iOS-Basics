75e864ae5ea3e3ec9f27617a4ca5e957312da601
Md Abir Hossain <hossain.mdabir@gmail.com>
2022-04-04 20:31:30 +0600

    I am Rich & I am Poor App

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by MD Abir Hossain on 4/4/22.
+//  Copyright © 2022 MD Abir Hossain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
